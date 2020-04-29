54bd133d3861a0ad608178fcab52df9e73912dd0
shivanipawar2904 <64489760+shivanipawar2904@users.noreply.github.com>
2020-04-29 11:25:56 +0530

    Add files via upload

---
+++ build/precache-manifest.ba9f38dc4c7d0d40974a9306b1bf1a12.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9619d2b0e6fa35803156d6236a4287cc",
+    "url": "/happy-birthday/index.html"
+  },
+  {
+    "revision": "5eedfe3328663e5f7379",
+    "url": "/happy-birthday/static/css/main.3c030577.chunk.css"
+  },
+  {
+    "revision": "f2aecb0da328660f0b29",
+    "url": "/happy-birthday/static/js/2.9fa3dddd.chunk.js"
+  },
+  {
+    "revision": "5eedfe3328663e5f7379",
+    "url": "/happy-birthday/static/js/main.a409872e.chunk.js"
+  },
+  {
+    "revision": "f984a2eaddb9e8d5ee2c",
+    "url": "/happy-birthday/static/js/runtime~main.b1b52b95.js"
+  },
+  {
+    "revision": "244d11534518cd0bac3955d27a4addda",
+    "url": "/happy-birthday/static/media/a1.244d1153.jpg"
+  },
+  {
+    "revision": "b51119be3b4da0bf6001fe81b6e2e6a5",
+    "url": "/happy-birthday/static/media/a2.b51119be.jpg"
+  },
+  {
+    "revision": "6f3726f2300277cfc59aa7ba6dc38d3f",
+    "url": "/happy-birthday/static/media/b.6f3726f2.jpg"
+  },
+  {
+    "revision": "705cc337d757ce7c85a29d0fac08bd61",
+    "url": "/happy-birthday/static/media/d.705cc337.jpg"
+  },
+  {
+    "revision": "0ab4c4de2c21d3c7f7f04c5df71fecd4",
+    "url": "/happy-birthday/static/media/h1.0ab4c4de.jpg"
+  },
+  {
+    "revision": "9e1830e27c6fb9359a0a8f578182b8fb",
+    "url": "/happy-birthday/static/media/h2.9e1830e2.jpg"
+  },
+  {
+    "revision": "c8ba42e57ef23f1d7b565a61d865aff3",
+    "url": "/happy-birthday/static/media/i.c8ba42e5.jpg"
+  },
+  {
+    "revision": "27a991597d938bd4e88d397be7f37ba6",
+    "url": "/happy-birthday/static/media/kazdy_rano.27a99159.mp3"
+  },
+  {
+    "revision": "bcbe89d5d40bee4158947cd74118feed",
+    "url": "/happy-birthday/static/media/p1.bcbe89d5.jpg"
+  },
+  {
+    "revision": "c4310703959218ef289e7d906c886f92",
+    "url": "/happy-birthday/static/media/p2.c4310703.jpg"
+  },
+  {
+    "revision": "74a5727fa11c938eaa0746d035a69009",
+    "url": "/happy-birthday/static/media/r.74a5727f.jpg"
+  },
+  {
+    "revision": "4740473b32d385ea54936d095c358f16",
+    "url": "/happy-birthday/static/media/t.4740473b.jpg"
+  },
+  {
+    "revision": "4142e83e1a286513923f5c11ea666505",
+    "url": "/happy-birthday/static/media/y1.4142e83e.jpg"
+  },
+  {
+    "revision": "626cf1fbac7252226680e8a265501118",
+    "url": "/happy-birthday/static/media/y2.626cf1fb.jpg"
+  }
+]);
